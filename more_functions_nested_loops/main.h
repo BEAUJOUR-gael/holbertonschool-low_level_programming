8de808562150883c88330c8f556ed207bf9a892b
gael <12202@holbertonschool.com>
2025-10-30 14:45:54 +0100

    Fix prototype and header for _isdigit

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,13 +3,6 @@
 
 int _putchar(char c);
 int _isupper(int c);
-
-#endif /* MAIN_H */
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-int _isupper(int c);
 int _isdigit(int c);
 
 #endif /* MAIN_H */
