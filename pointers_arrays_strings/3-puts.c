35039856cd9d66565cf3928b3458bc1b75affe76
gael <12202@holbertonschool.com>
2025-10-30 15:26:36 +0100

    Add _puts function

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - affiche une chaîne de caractères suivie d'une nouvelle ligne
+ * @str: pointeur vers la chaîne à afficher
+ *
+ * Return: rien (void)
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
