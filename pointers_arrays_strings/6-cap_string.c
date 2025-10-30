253bdcb1861df122c9569fd3c7da3da0b225edd3
gael <12202@holbertonschool.com>
2025-10-30 16:40:22 +0100

    Add cap_string function

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - met en majuscule la première lettre de chaque mot
+ * @str: chaîne de caractères à modifier
+ *
+ * Return: pointeur vers la chaîne modifiée
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (str[i] != '\0')
+	{
+		/* Si c'est la première lettre du mot */
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] -= 32;
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
+
